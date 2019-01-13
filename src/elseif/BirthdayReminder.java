87d1aac190e2980dcf72b4f976f0d12c8aacb21a
league <league@imac-6.attlocal.net>
2019-01-12 17:27:11 -0800

    I did birthday reminder today!

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,21 +11,33 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "September 18th";
+		String dadsBirthday = "January 5th";
+		String myBirthday = "March 19th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String Birthday=JOptionPane.showInputDialog(null, "Who's birthday do you want?");
 		// 3. Print out what the user typed
-		
+	System.out.println(Birthday);
 		// 4. if user asked for "mom"
 			//print mom's birthday
-		// 5. if user asked for "dad"
+		if (Birthday.equals("mom")) {
+		System.out.println(momsBirthday);
+		}
+	// 5. if user asked for "dad"
 			// print dad's birthday
+		else if (Birthday.equals("dad")) {
+			System.out.println(dadsBirthday);
+			}
 		// 6. if user asked for your name
 			// print myBirthday
+		else if (Birthday.equals("yours")) {
+			System.out.println(myBirthday);
+		}
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		else {
+		System.out.println("Sorry I don't know that person's birthday!");
+		}
+			
 	} 
 }
