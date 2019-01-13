bbc5c4fdb3540a20af61ec0fd995a45316a4170e
league <league@wts9.attlocal.net>
2019-01-13 11:29:38 -0800

    I did AreYouHappy this class

---
+++ src/elseif/AreYouHappy.java
@@ -0,0 +1,32 @@
+package elseif;
+
+import javax.swing.JOptionPane;
+
+public class AreYouHappy {
+public static void main(String[] args) {
+String AreYouHappy=JOptionPane.showInputDialog(null, "Are You Happy?");
+
+if (AreYouHappy.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "Then Keep doing whatever your doing.");
+}
+else if (AreYouHappy.equals("no")) {
+	String DoYouWantToBeHappy=JOptionPane.showInputDialog("Do you want to be happy?");
+
+ if (DoYouWantToBeHappy.equals("Yes")) {
+	JOptionPane.showMessageDialog(null, "Then change something");
+}
+ else if (DoYouWantToBeHappy.equals("no")) {
+	 JOptionPane.showMessageDialog(null, "Keep Doing Whatever your doing.");
+	 
+ }
+}	
+	
+	
+	
+	
+	
+	
+	
+
+}
+}
