2c9cb78ab7618731b2d2ce0e92860eec7991840b
KingArman999 <44267696+KingArman999@users.noreply.github.com>
2019-02-02 11:26:29 -0800

    I did Robot walk of fame today!

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -8,11 +8,29 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		Robot Bob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		Bob.setX(150);
+		Bob.setY(600);
 		// 2. Make the robot draw a star shape. Hint: 144.
+		
+		Bob.setSpeed(100000000);
+		for (int i=0; i<100; i++) {
+		
+		for (int N=0; N<5; N++) {
+		Bob.penDown();
+		Bob.move(30);	
+		Bob.turn(144);
+		}
+		Bob.penUp();
+		Bob.move(30);
+	
+		
+						
+				
+	}	
+
 
 		// 3. Set the size of the star to 30.
 
